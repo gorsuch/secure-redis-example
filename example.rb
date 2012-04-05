d7e08e840bcc41dbc99175dd854ef939124f6938
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-04-05 10:33:25 -0500

    ruby 1.8.7 compat

--- example.rb
+++ example.rb
@@ -6,8 +6,9 @@ standard_url = "redis://somehost.local:6379"
 # assumes stunnel running on same host, bound to port 6380
 secure_url = "redis+ssl://somehost.local:6380"
 
-standard_client = Redis.new url: standard_url
+standard_client = Redis.new :url => standard_url
 puts standard_client.info
 
-secure_client = Redis.new url: secure_url
+secure_client = Redis.new :url => secure_url
 puts secure_client.info
+
