cd0a9aefae2d47b619e69efabfd72db612a05504
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-04-05 10:08:07 -0500

    cleaner

--- example.rb
+++ example.rb
@@ -1,9 +1,13 @@
 require 'redis'
 require 'redis/connection/universal'
 
-standard = Redis.new url: ENV['STANDARD_REDIS_URL']
-puts standard.info
+# standard redis url
+standard_url = "redis://somehost.local:6379"
+# assumes stunnel running on same host, bound to port 6380
+secure_url = "redis+ssl://somehost.local:6380"
 
-secure = Redis.new url: ENV['SECURE_REDIS_URL']
-puts secure.info
+standard_client = Redis.new url: standard_url
+puts standard_client.info
 
+secure_client = Redis.new url: secure_url
+puts secure_client.info
